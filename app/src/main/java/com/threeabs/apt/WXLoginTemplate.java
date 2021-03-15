d15e6b39dd9a20ae33759dfeb6336fc4024c224f
ThreeAbs <36951878+ThreeAbs@users.noreply.github.com>
2021-03-15 13:57:00 +0800

    第6次提交

--- app/src/main/java/com/threeabs/apt/WXLoginTemplate.java
+++ app/src/main/java/com/threeabs/apt/WXLoginTemplate.java
@@ -1,13 +1,12 @@
 package com.threeabs.apt;
 
-import com.threeabs.annotation.TargetTemplate;
 
 /**
  * Author: HD on 2021/3/12
  * Email: zhanghd@aura.cn
  * Describe:
  */
-@TargetTemplate(targetPath = "com.login.test", targetName = "WXEntryActivity", targetTemplate = WXLoginTemplate.class)
+//@TargetTemplate(targetPath = "com.login", targetName = "WXEntryActivity", targetTemplate = WXLoginTemplate.class)
 public class WXLoginTemplate {
 
 }
